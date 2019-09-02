b2e4e8337b8972e286dba194c046a1bd4ee84215
Nikeweke <Driver3@meta.ua>
2019-09-02 16:30:29 +0300

    Update packages_linux.sh

--- packages_runners/packages_linux.sh
+++ packages_runners/packages_linux.sh
@@ -3,27 +3,6 @@ export PATH=$PATH:$GOPATH/bin
 
 echo Loading packages...
 
-go get -u github.com/labstack/echo
-echo Echo is loaded
-
-go get github.com/fatih/color
-echo Color is loaded
-
-go get github.com/jessevdk/go-assets
-echo Go-assets is loaded
+go get github.com/pilu/fresh
 
 echo Packages loaded successfuly!
-
-# go get github.com/jessevdk/go-assets-builder
-# go get github.com/dgrijalva/jwt-go
-# go get github.com/pilu/fresh
-# go get github.com/robfig/cron
-# go get github.com/jessevdk/go-assets-builder
-
-# go get github.com/jinzhu/gorm
-# go get github.com/jinzhu/gorm/dialects/mysql
-# go get github.com/jinzhu/gorm/dialects/sqlite
-# go get github.com/jinzhu/gorm/dialects/mssql
-# go get github.com/jinzhu/gorm/dialects/postgres
-
-# go get gopkg.in/mgo.v2
